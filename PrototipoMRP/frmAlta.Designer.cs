4f73e6ca296b09ffe3c10559c7417fc42f6aa35a
BrianSanti <briansanti80@gmail.com>
2020-09-11 18:05:42 -0600

    Hola

---
+++ PrototipoMRP/frmAlta.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PrototipoMRP
+{
+    partial class frmAlta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblprodcuto = new System.Windows.Forms.Label();
+            this.lblclave = new System.Windows.Forms.Label();
+            this.lblalmacen = new System.Windows.Forms.Label();
+            this.lblcontrol = new System.Windows.Forms.Label();
+            this.lblcantidades = new System.Windows.Forms.Label();
+            this.lblstockm = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblpendientes = new System.Windows.Forms.Label();
+            this.lblstockmi = new System.Windows.Forms.Label();
+            this.cmbclave = new System.Windows.Forms.ComboBox();
+            this.cmbalmacen = new System.Windows.Forms.ComboBox();
+            this.txtcontrol = new System.Windows.Forms.TextBox();
+            this.txtstockm = new System.Windows.Forms.TextBox();
+            this.txtstockmi = new System.Windows.Forms.TextBox();
+            this.txtexistencia = new System.Windows.Forms.TextBox();
+            this.txtpendientes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblprodcuto
+            // 
+            this.lblprodcuto.AutoSize = true;
+            this.lblprodcuto.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprodcuto.Location = new System.Drawing.Point(99, 23);
+            this.lblprodcuto.Name = "lblprodcuto";
+            this.lblprodcuto.Size = new System.Drawing.Size(80, 20);
+            this.lblprodcuto.TabIndex = 3;
+            this.lblprodcuto.Text = "Producto";
+            // 
+            // lblclave
+            // 
+            this.lblclave.AutoSize = true;
+            this.lblclave.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblclave.Location = new System.Drawing.Point(26, 66);
+            this.lblclave.Name = "lblclave";
+            this.lblclave.Size = new System.Drawing.Size(60, 20);
+            this.lblclave.TabIndex = 4;
+            this.lblclave.Text = "Clave:";
+            // 
+            // lblalmacen
+            // 
+            this.lblalmacen.AutoSize = true;
+            this.lblalmacen.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblalmacen.Location = new System.Drawing.Point(26, 112);
+            this.lblalmacen.Name = "lblalmacen";
+            this.lblalmacen.Size = new System.Drawing.Size(84, 20);
+            this.lblalmacen.TabIndex = 5;
+            this.lblalmacen.Text = "Almacen:";
+            // 
+            // lblcontrol
+            // 
+            this.lblcontrol.AutoSize = true;
+            this.lblcontrol.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontrol.Location = new System.Drawing.Point(317, 112);
+            this.lblcontrol.Name = "lblcontrol";
+            this.lblcontrol.Size = new System.Drawing.Size(148, 20);
+            this.lblcontrol.TabIndex = 6;
+            this.lblcontrol.Text = "Control Almacen:";
+            // 
+            // lblcantidades
+            // 
+            this.lblcantidades.AutoSize = true;
+            this.lblcantidades.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcantidades.Location = new System.Drawing.Point(99, 179);
+            this.lblcantidades.Name = "lblcantidades";
+            this.lblcantidades.Size = new System.Drawing.Size(98, 20);
+            this.lblcantidades.TabIndex = 7;
+            this.lblcantidades.Text = "Cantidades";
+            // 
+            // lblstockm
+            // 
+            this.lblstockm.AutoSize = true;
+            this.lblstockm.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblstockm.Location = new System.Drawing.Point(26, 244);
+            this.lblstockm.Name = "lblstockm";
+            this.lblstockm.Size = new System.Drawing.Size(124, 20);
+            this.lblstockm.TabIndex = 8;
+            this.lblstockm.Text = "Stock Maximo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Existencia:";
+            // 
+            // lblpendientes
+            // 
+            this.lblpendientes.AutoSize = true;
+            this.lblpendientes.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpendientes.Location = new System.Drawing.Point(328, 298);
+            this.lblpendientes.Name = "lblpendientes";
+            this.lblpendientes.Size = new System.Drawing.Size(193, 20);
+            this.lblpendientes.TabIndex = 10;
+            this.lblpendientes.Text = "Pendientes por Recibir:";
+            // 
+            // lblstockmi
+            // 
+            this.lblstockmi.AutoSize = true;
+            this.lblstockmi.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblstockmi.Location = new System.Drawing.Point(328, 244);
+            this.lblstockmi.Name = "lblstockmi";
+            this.lblstockmi.Size = new System.Drawing.Size(121, 20);
+            this.lblstockmi.TabIndex = 11;
+            this.lblstockmi.Text = "Stock Minimo:";
+            // 
+            // cmbclave
+            // 
+            this.cmbclave.FormattingEnabled = true;
+            this.cmbclave.Location = new System.Drawing.Point(123, 68);
+            this.cmbclave.Name = "cmbclave";
+            this.cmbclave.Size = new System.Drawing.Size(482, 24);
+            this.cmbclave.TabIndex = 12;
+            // 
+            // cmbalmacen
+            // 
+            this.cmbalmacen.FormattingEnabled = true;
+            this.cmbalmacen.Location = new System.Drawing.Point(123, 112);
+            this.cmbalmacen.Name = "cmbalmacen";
+            this.cmbalmacen.Size = new System.Drawing.Size(188, 24);
+            this.cmbalmacen.TabIndex = 13;
+            // 
+            // txtcontrol
+            // 
+            this.txtcontrol.Location = new System.Drawing.Point(471, 112);
+            this.txtcontrol.Name = "txtcontrol";
+            this.txtcontrol.Size = new System.Drawing.Size(134, 22);
+            this.txtcontrol.TabIndex = 14;
+            // 
+            // txtstockm
+            // 
+            this.txtstockm.Location = new System.Drawing.Point(156, 244);
+            this.txtstockm.Name = "txtstockm";
+            this.txtstockm.Size = new System.Drawing.Size(155, 22);
+            this.txtstockm.TabIndex = 15;
+            // 
+            // txtstockmi
+            // 
+            this.txtstockmi.Location = new System.Drawing.Point(455, 244);
+            this.txtstockmi.Name = "txtstockmi";
+            this.txtstockmi.Size = new System.Drawing.Size(150, 22);
+            this.txtstockmi.TabIndex = 16;
+            // 
+            // txtexistencia
+            // 
+            this.txtexistencia.Location = new System.Drawing.Point(131, 296);
+            this.txtexistencia.Name = "txtexistencia";
+            this.txtexistencia.Size = new System.Drawing.Size(180, 22);
+            this.txtexistencia.TabIndex = 17;
+            // 
+            // txtpendientes
+            // 
+            this.txtpendientes.Location = new System.Drawing.Point(527, 296);
+            this.txtpendientes.Name = "txtpendientes";
+            this.txtpendientes.Size = new System.Drawing.Size(78, 22);
+            this.txtpendientes.TabIndex = 18;
+            // 
+            // frmAlta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(682, 423);
+            this.Controls.Add(this.txtpendientes);
+            this.Controls.Add(this.txtexistencia);
+            this.Controls.Add(this.txtstockmi);
+            this.Controls.Add(this.txtstockm);
+            this.Controls.Add(this.txtcontrol);
+            this.Controls.Add(this.cmbalmacen);
+            this.Controls.Add(this.cmbclave);
+            this.Controls.Add(this.lblstockmi);
+            this.Controls.Add(this.lblpendientes);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblstockm);
+            this.Controls.Add(this.lblcantidades);
+            this.Controls.Add(this.lblcontrol);
+            this.Controls.Add(this.lblalmacen);
+            this.Controls.Add(this.lblclave);
+            this.Controls.Add(this.lblprodcuto);
+            this.Name = "frmAlta";
+            this.Text = "frmAlta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblprodcuto;
+        private System.Windows.Forms.Label lblclave;
+        private System.Windows.Forms.Label lblalmacen;
+        private System.Windows.Forms.Label lblcontrol;
+        private System.Windows.Forms.Label lblcantidades;
+        private System.Windows.Forms.Label lblstockm;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblpendientes;
+        private System.Windows.Forms.Label lblstockmi;
+        private System.Windows.Forms.ComboBox cmbclave;
+        private System.Windows.Forms.ComboBox cmbalmacen;
+        private System.Windows.Forms.TextBox txtcontrol;
+        private System.Windows.Forms.TextBox txtstockm;
+        private System.Windows.Forms.TextBox txtstockmi;
+        private System.Windows.Forms.TextBox txtexistencia;
+        private System.Windows.Forms.TextBox txtpendientes;
+    }
+}
---
+++ PrototipoMRP/frmProductos.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PrototipoMRP
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblclave = new System.Windows.Forms.Label();
+            this.lblalterna = new System.Windows.Forms.Label();
+            this.lbldescripcion = new System.Windows.Forms.Label();
+            this.pbximagen = new System.Windows.Forms.PictureBox();
+            this.txtalterna = new System.Windows.Forms.TextBox();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.cmbclave = new System.Windows.Forms.ComboBox();
+            this.btnayuda = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.btnaceptar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbximagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblclave
+            // 
+            this.lblclave.AutoSize = true;
+            this.lblclave.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblclave.Location = new System.Drawing.Point(71, 83);
+            this.lblclave.Name = "lblclave";
+            this.lblclave.Size = new System.Drawing.Size(60, 20);
+            this.lblclave.TabIndex = 3;
+            this.lblclave.Text = "Clave:";
+            // 
+            // lblalterna
+            // 
+            this.lblalterna.AutoSize = true;
+            this.lblalterna.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblalterna.Location = new System.Drawing.Point(71, 132);
+            this.lblalterna.Name = "lblalterna";
+            this.lblalterna.Size = new System.Drawing.Size(119, 20);
+            this.lblalterna.TabIndex = 4;
+            this.lblalterna.Text = "Clave alterna:";
+            // 
+            // lbldescripcion
+            // 
+            this.lbldescripcion.AutoSize = true;
+            this.lbldescripcion.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldescripcion.Location = new System.Drawing.Point(71, 187);
+            this.lbldescripcion.Name = "lbldescripcion";
+            this.lbldescripcion.Size = new System.Drawing.Size(109, 20);
+            this.lbldescripcion.TabIndex = 5;
+            this.lbldescripcion.Text = "Descripcion:";
+            // 
+            // pbximagen
+            // 
+            this.pbximagen.BackColor = System.Drawing.Color.White;
+            this.pbximagen.Location = new System.Drawing.Point(401, 241);
+            this.pbximagen.Name = "pbximagen";
+            this.pbximagen.Size = new System.Drawing.Size(327, 168);
+            this.pbximagen.TabIndex = 6;
+            this.pbximagen.TabStop = false;
+            // 
+            // txtalterna
+            // 
+            this.txtalterna.Location = new System.Drawing.Point(226, 132);
+            this.txtalterna.Name = "txtalterna";
+            this.txtalterna.Size = new System.Drawing.Size(502, 22);
+            this.txtalterna.TabIndex = 8;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(226, 185);
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(502, 22);
+            this.txtdescripcion.TabIndex = 9;
+            // 
+            // cmbclave
+            // 
+            this.cmbclave.FormattingEnabled = true;
+            this.cmbclave.Location = new System.Drawing.Point(226, 83);
+            this.cmbclave.Name = "cmbclave";
+            this.cmbclave.Size = new System.Drawing.Size(502, 24);
+            this.cmbclave.TabIndex = 10;
+            // 
+            // btnayuda
+            // 
+            this.btnayuda.BackColor = System.Drawing.Color.White;
+            this.btnayuda.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnayuda.Location = new System.Drawing.Point(224, 328);
+            this.btnayuda.Name = "btnayuda";
+            this.btnayuda.Size = new System.Drawing.Size(129, 53);
+            this.btnayuda.TabIndex = 15;
+            this.btnayuda.Text = "Ayuda";
+            this.btnayuda.UseVisualStyleBackColor = false;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.BackColor = System.Drawing.Color.White;
+            this.btnguardar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnguardar.Location = new System.Drawing.Point(89, 328);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(129, 53);
+            this.btnguardar.TabIndex = 14;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = false;
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.White;
+            this.btncancelar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancelar.Location = new System.Drawing.Point(224, 269);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(129, 53);
+            this.btncancelar.TabIndex = 13;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnaceptar
+            // 
+            this.btnaceptar.BackColor = System.Drawing.Color.White;
+            this.btnaceptar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnaceptar.Location = new System.Drawing.Point(89, 269);
+            this.btnaceptar.Name = "btnaceptar";
+            this.btnaceptar.Size = new System.Drawing.Size(129, 53);
+            this.btnaceptar.TabIndex = 12;
+            this.btnaceptar.Text = "Aceptar";
+            this.btnaceptar.UseVisualStyleBackColor = false;
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnayuda);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btnaceptar);
+            this.Controls.Add(this.cmbclave);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.txtalterna);
+            this.Controls.Add(this.pbximagen);
+            this.Controls.Add(this.lbldescripcion);
+            this.Controls.Add(this.lblalterna);
+            this.Controls.Add(this.lblclave);
+            this.Name = "frmProductos";
+            this.Text = "frmProductos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbximagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblclave;
+        private System.Windows.Forms.Label lblalterna;
+        private System.Windows.Forms.Label lbldescripcion;
+        private System.Windows.Forms.PictureBox pbximagen;
+        private System.Windows.Forms.TextBox txtalterna;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.ComboBox cmbclave;
+        private System.Windows.Forms.Button btnayuda;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.Button btnaceptar;
+    }
+}
