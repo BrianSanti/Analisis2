f2e47a2e403cf1106f49b3ad6cd57775035e20c0
BrianSanti <briansanti80@gmail.com>
2020-09-29 14:40:53 -0600

    Ya

---
+++ PrototipoMRP/frmCompleta.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PrototipoMRP
+{
+    partial class frmCompleta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCompleta));
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.btnEntradas = new System.Windows.Forms.Button();
+            this.btnOrdenes = new System.Windows.Forms.Button();
+            this.pnl2 = new System.Windows.Forms.Panel();
+            this.pnl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pnl1.Controls.Add(this.btnEntradas);
+            this.pnl1.Controls.Add(this.btnOrdenes);
+            this.pnl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl1.Location = new System.Drawing.Point(0, 0);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(1924, 85);
+            this.pnl1.TabIndex = 0;
+            this.pnl1.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl1_Paint);
+            // 
+            // btnEntradas
+            // 
+            this.btnEntradas.BackColor = System.Drawing.Color.White;
+            this.btnEntradas.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnEntradas.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntradas.Location = new System.Drawing.Point(264, 0);
+            this.btnEntradas.Name = "btnEntradas";
+            this.btnEntradas.Size = new System.Drawing.Size(264, 85);
+            this.btnEntradas.TabIndex = 1;
+            this.btnEntradas.Text = "Entradas";
+            this.btnEntradas.UseVisualStyleBackColor = false;
+            this.btnEntradas.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnOrdenes
+            // 
+            this.btnOrdenes.BackColor = System.Drawing.Color.White;
+            this.btnOrdenes.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnOrdenes.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenes.Location = new System.Drawing.Point(0, 0);
+            this.btnOrdenes.Name = "btnOrdenes";
+            this.btnOrdenes.Size = new System.Drawing.Size(264, 85);
+            this.btnOrdenes.TabIndex = 0;
+            this.btnOrdenes.Text = "Ordenes";
+            this.btnOrdenes.UseVisualStyleBackColor = false;
+            this.btnOrdenes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pnl2
+            // 
+            this.pnl2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnl2.BackgroundImage")));
+            this.pnl2.Location = new System.Drawing.Point(158, 91);
+            this.pnl2.Name = "pnl2";
+            this.pnl2.Size = new System.Drawing.Size(1172, 774);
+            this.pnl2.TabIndex = 1;
+            // 
+            // frmCompleta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.pnl2);
+            this.Controls.Add(this.pnl1);
+            this.Name = "frmCompleta";
+            this.Text = "frmCompleta";
+            this.pnl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Button btnOrdenes;
+        private System.Windows.Forms.Button btnEntradas;
+        private System.Windows.Forms.Panel pnl2;
+    }
+}
---
+++ PrototipoMRP/frmProductos.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace PrototipoMRP
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClave = new System.Windows.Forms.Label();
+            this.lblAlterna = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            this.txtAlterna = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.cmbClave = new System.Windows.Forms.ComboBox();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.Location = new System.Drawing.Point(71, 83);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(60, 20);
+            this.lblClave.TabIndex = 3;
+            this.lblClave.Text = "Clave:";
+            // 
+            // lblAlterna
+            // 
+            this.lblAlterna.AutoSize = true;
+            this.lblAlterna.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlterna.Location = new System.Drawing.Point(71, 132);
+            this.lblAlterna.Name = "lblAlterna";
+            this.lblAlterna.Size = new System.Drawing.Size(119, 20);
+            this.lblAlterna.TabIndex = 4;
+            this.lblAlterna.Text = "Clave alterna:";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(71, 187);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(109, 20);
+            this.lblDescripcion.TabIndex = 5;
+            this.lblDescripcion.Text = "Descripcion:";
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.BackColor = System.Drawing.Color.White;
+            this.pbxImagen.Location = new System.Drawing.Point(401, 241);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(327, 168);
+            this.pbxImagen.TabIndex = 6;
+            this.pbxImagen.TabStop = false;
+            // 
+            // txtAlterna
+            // 
+            this.txtAlterna.Location = new System.Drawing.Point(226, 132);
+            this.txtAlterna.Name = "txtAlterna";
+            this.txtAlterna.Size = new System.Drawing.Size(502, 22);
+            this.txtAlterna.TabIndex = 8;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(226, 185);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(502, 22);
+            this.txtDescripcion.TabIndex = 9;
+            // 
+            // cmbClave
+            // 
+            this.cmbClave.FormattingEnabled = true;
+            this.cmbClave.Location = new System.Drawing.Point(226, 83);
+            this.cmbClave.Name = "cmbClave";
+            this.cmbClave.Size = new System.Drawing.Size(502, 24);
+            this.cmbClave.TabIndex = 10;
+            // 
+            // btnAyuda
+            // 
+            this.btnAyuda.BackColor = System.Drawing.Color.White;
+            this.btnAyuda.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAyuda.Location = new System.Drawing.Point(224, 328);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(129, 53);
+            this.btnAyuda.TabIndex = 15;
+            this.btnAyuda.Text = "Ayuda";
+            this.btnAyuda.UseVisualStyleBackColor = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.White;
+            this.btnGuardar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(89, 328);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(129, 53);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(224, 269);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(129, 53);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.White;
+            this.btnAceptar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(89, 269);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(129, 53);
+            this.btnAceptar.TabIndex = 12;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAyuda);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cmbClave);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtAlterna);
+            this.Controls.Add(this.pbxImagen);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblAlterna);
+            this.Controls.Add(this.lblClave);
+            this.Name = "frmProductos";
+            this.Text = "frmProductos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.Label lblAlterna;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.PictureBox pbxImagen;
+        private System.Windows.Forms.TextBox txtAlterna;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.ComboBox cmbClave;
+        private System.Windows.Forms.Button btnAyuda;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+    }
+}
